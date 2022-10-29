894f93af5fc89d73a19603e799670526ab268aeb
Crystal Redmon <cato2848@gmail.com>
2022-10-29 16:38:30 -0500

    begin feedback component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import Feedback from '../Feedback/Feedback';
+
+
 
 function App() {
 
+  /// useEffect for onload page
+  useEffect(() => {
+
+    /// GET method
+    /// UNSURE WHAT I'M SUPPOSE TO BE GETTING AT THIS POINT
+    /// SINCE THERE IS NO FEEDBACK TO GET YET.
+    /// SHOULD I HAVE QUESTIONS IN THE DB/REDUX STORE
+
+
+  })
+
+
+
+  /// ESTABLISH ROUTES
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+
+        <Route>
+          {/* path='/feedback/:id' */}
+          <Feedback />
+        </Route>
+
+      </div>
+    </Router>
   );
 }
 
